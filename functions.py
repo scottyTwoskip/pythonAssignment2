4a9752b1dedb6cd314ae2f16cc60c4a7fde53897
scottyTwoskip <scottyeansor@gmail.com>
2023-12-07 08:11:18 -0700

    finished assignment 2

--- functions.py
+++ functions.py
@@ -32,6 +32,34 @@ string_example = "yo dude"
 print(f"the reversed string is: {rev_string(string_example)}")
 
 #function #4
+def num_within(number, start, end):
+        return start <= number <= end
 
+num_within_example= num_within(3, 2, 4)
+print(f"an example is of a num within: {num_within_example}")
+
+#function 5
+#n is number
+def pascal(n):
+    if n <= 0:
+        return
+    
+    row = [1]
+
+    for i in range(n):
+        print(" " * (n - i), end="")
+        for num in row:
+            print(num, end=" ")
+        print()
+
+        # Calculate the next row
+        next_row = [1]
+        for j in range(len(row) - 1):
+            next_num = row[j] + row[j + 1]
+            next_row.append(next_num)
+        next_row.append(1)
+        row = next_row
+
+pascal(5)
 
 
